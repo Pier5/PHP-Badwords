27e50307e44ca7ac15ae524425bac95eb1a067eb
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-07 19:49:36 +0200

    -stampa testo e lunghezza-

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    
+    <?php
+        $testo_lungo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia omnis necessitatibus ipsam itaque reiciendis libero similique dolore ipsum dignissimos ut nihil culpa iusto velit et consequuntur voluptates, iste quod quibusdam."
+    ?>
+    <h1>Testo: <?php echo $testo_lungo;?></h1>
+    <h2>Il testo è lungo: <?php echo strlen($testo_lungo);?> caratteri</h2>
 </body>
 </html>
