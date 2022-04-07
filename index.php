e8ea3635cd2b498594c2692e098cb5016df1fecd
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-04-07 20:09:35 +0200

    -testo censurato-

--- index.php
+++ index.php
@@ -7,10 +7,29 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    <?php
-        $testo_lungo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia omnis necessitatibus ipsam itaque reiciendis libero similique dolore ipsum dignissimos ut nihil culpa iusto velit et consequuntur voluptates, iste quod quibusdam."
-    ?>
-    <h1>Testo: <?php echo $testo_lungo;?></h1>
+
+    <!-- stampa testo e lunghezza testo -->
+    <h1>Testo:</h1>
+    <h2>
+        <?php 
+            $testo_lungo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia omnis necessitatibus ipsam itaque reiciendis libero similique dolore ipsum dignissimos ut nihil culpa iusto velit et consequuntur voluptates, iste quod quibusdam.";
+            echo $testo_lungo;
+        ?>
+    </h2>
     <h2>Il testo è lungo: <?php echo strlen($testo_lungo);?> caratteri</h2>
+
+    <hr>
+
+    <!-- stampa testo con parola che viene censurata e nuova lunghezza -->
+    <h1>Testo censurato:</h1>
+    <h2>
+        <?php
+            $bad_word = $_GET["badword"];
+            $testo_censurato = str_replace($bad_word, "***", $testo_lungo);
+            echo $testo_censurato;
+        ?>
+    </h2>
+    <h2>Il testo è lungo: <?php echo strlen($testo_censurato);?> caratteri</h2>
+
 </body>
 </html>
